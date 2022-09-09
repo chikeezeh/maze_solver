c6126649cc0a511f68e4b8a3c2cdf2f4f2760cbd
Chike Ezeh <cezeh@tulane.edu>
2022-09-08 21:46:14 -0700

    added draw_move to cell.py

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from graphics import Window, Line, Point
+from cell import Cell
 
 def main():
-    win = Window(800,800)
-    l = Line(Point(50,50), Point(500,500))
-    win.draw_line(l, "black")
+    win = Window(800, 600)
+
+    c = Cell(win)
+    c.left_wall = True
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.right_wall = True
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.bottom_wall = True
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.draw(300, 300, 500, 500)
+
     win.wait_for_close()
+
 
 main()
