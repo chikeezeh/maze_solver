aa22f5bea8263145ae1d1808a39362ad76a2e47f
Chike Ezeh <cezeh@tulane.edu>
2022-09-09 20:43:33 -0700

    added the functionality of random generated maze

--- cell.py
+++ cell.py
@@ -7,6 +7,7 @@ class Cell:
         self.right_wall = True
         self.top_wall = True
         self.bottom_wall = True
+        self.visited = False
         self._x1 = None
         self._x2 = None
         self._y1 = None
